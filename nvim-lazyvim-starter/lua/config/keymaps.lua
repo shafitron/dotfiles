9549124511bb2b393590b388ae3364bb2c5895ba
shafitron <shafitron@gmail.com>
2024-01-13 20:45:12 -0700

    borrowed discipline code from craftzdog/devaslife and folke
    This is pretty cool for starters. But I plan to swap it out with hardtime.nvim
    [m4xshen/hardtime.nvim: A Neovim plugin helping you establish good command workflow and habit](https://github.com/m4xshen/hardtime.nvim)

--- nvim-lazyvim-starter/lua/config/keymaps.lua
+++ nvim-lazyvim-starter/lua/config/keymaps.lua
@@ -1,6 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Borrowed discipline code from craftzdog/devaslife
+local discipline = require("shafitron.discipline")
+discipline.cowboy()
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
